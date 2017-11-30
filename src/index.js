88169f1e57f54d3a3f54b9629b08134497b33698
jun.ho <188630703@qq.com>
2017-11-30 22:56:10 +0800

    fix: git目录

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';// 为什么不可以是router-dom? 觉得不一定要关联react
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 /*reducers*/
-import reducers from "./reducers/index";
+import reducersRender from "./reducers/index";
 /*routes*/
 import routes from "./routes";
 
@@ -17,25 +17,26 @@ import rootSaga from "./saga/rank";
 window.actionMode = 'sagaMode';
 // 需要routerMiddleware中间件才能给store.disptach方法传递react-router-redux的action
 import ReduxThunk from 'redux-thunk';
-const middleware = routerMiddleware(hashHistory);
+const hashRouterMiddleware = routerMiddleware(hashHistory);
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
-	reducers,
+  reducersRender,
+  // 插入异步管理系统与路由系统, 提供修改状态的机会
   applyMiddleware(
-    middleware,
+    hashRouterMiddleware,
     (window.actionMode === 'sagaMode'? sagaMiddleware: ReduxThunk)
   )
 );
-// 运行saga管理
+// 运行异步管理系统
 window.actionMode === 'sagaMode' && sagaMiddleware.run(rootSaga);
 
 const history = syncHistoryWithStore(hashHistory, store);
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router history={history}>
-			{routes}
-		</Router>
-	</Provider>,
-	document.getElementById('rC')
+  <Provider store={store}>
+    <Router history={history}>
+      {routes}
+    </Router>
+  </Provider>,
+  document.getElementById('rC')
 );
